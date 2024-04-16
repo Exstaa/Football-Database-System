e6ce1f3b7931d18fb46849f1f297632dc46ad421
Exstaa <exstagithub@gmail.com>
2024-04-16 20:01:02 +0400

    Main class is created.

---
+++ SpringFootballDatabaseClass.java
@@ -0,0 +1,12 @@
+package az.hafizrzazade.footballdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringFootballDatabaseClass {
+	   public static void main(String[] args) {
+		   SpringApplication.run(SpringFootballDatabaseClass.class, args);
+	   }
+}
